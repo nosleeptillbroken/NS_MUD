a52ac52c087c48b3c0967d2a8bab4537c7d7f70f
jthra037 <its_thrasher@live.ca>
2016-10-09 16:27:57 -0400

    Add assert to standard precompiled header. Add Winsock init Add Winsock socket creation for server

--- NS-MUD/NS-MUD/accept_thread.cpp
+++ NS-MUD/NS-MUD/accept_thread.cpp
@@ -5,6 +5,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <string>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -17,9 +18,44 @@ private:
 	// selector?
 	// connectionList?
 public:
+	/*
+	A lot of the code here (so far all) is from
+	https://msdn.microsoft.com/en-us/library/windows/desktop/ms740632(v=vs.85).aspx
+	*/
 	AcceptThread(int port) //also needs selector and connectionList
 	{
+		#define DEFAULT_PORT "27015" // it must be done this way for some reason
+	
+		WSADATA wsaData; // WSA - WinSockApplication Data from winsock2.h
+		struct addrinfo *result = NULL, *ptr = NULL, hints; // make addrinfo with result and ptr, and hints?
+		int iResult; // return from WSAStartup
 
+		// Initialize Winsock
+		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData); // Startup service using this function
+		if (iResult != 0) // Did not succeed
+		{
+			printf("WSAStartup failed: %d\n", iResult); // Give the result
+			assert(iResult != 0); // Crash out gracefully
+		}
+
+		// Create a socket for the server
+
+		// All this stuff is defined up in winsock2.h
+		// Legitimately I don't really know what most of this means
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+		hints.ai_flags = AI_PASSIVE;
+
+		// Resolve the local address and port to be used by the server
+		iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+		if (iResult != 0)
+		{
+			printf("getaddrinfo failed: %d\n", iResult);
+			WSACleanup();
+			assert(iResult != 0);
+		}
 	}
 
 };
